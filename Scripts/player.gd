fcd04bb239157b5d98c404b853a4d70d47a41e5a
Sakubo <alexis.gks.13@gmail.com>
2025-10-25 01:49:15 -0300

    add main scene and player

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,16 +4,21 @@ extends Node2D
 
 const MAX_HEALTH: int = 10
 
-@export var health: int = 10
-
-func _ready() -> void:
+@export var health: int = 10:
+	get:
+		return health
+	set(new_value):
+		health = clamp(new_value, 0, MAX_HEALTH)
+		update_health()
+	
+func _ready():
 	update_health()
 
+func update_health():
+	if not is_instance_valid(_health_label):
+		return
+	_health_label.text = str(health) + "/" + str(MAX_HEALTH)
+	
 func add_health(difference: int):
 	health += difference
-	health = clamp(health, 0, MAX_HEALTH)
 	
-	update_health()
-	
-func update_health():
-	_health_label.text = str(health) + "/" + str(MAX_HEALTH)
